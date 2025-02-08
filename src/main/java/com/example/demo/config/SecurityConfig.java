3b6c73c55573011216bb095b8d39b902aacbff93
Hussain <sidmohd0404@gmail.com>
2025-02-09 01:47:44 +0530

    Adding Authentication only on 'GET API'

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -21,7 +22,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF (enable in production)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/**").hasRole("SIDDIQUE") // Only ADMIN can access /api/users
+                        .requestMatchers(HttpMethod.GET,"/api/users/**").hasRole("SIDDIQUE") // Only ADMIN can access /api/users
                         .anyRequest().permitAll() // Allow all other requests
                 )
                 .httpBasic(httpBasic -> {}); // Enable HTTP Basic Authentication
